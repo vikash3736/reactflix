72071d8756e0e5b38ae722abb161c3c1233e2155
Vikash Kumar Mahto <emails2vikash@gmail.com>
2023-01-28 21:28:26 +0530

    Project started using redux template

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <div className='banner'>Banner</div>
+  )
+}
+
+export default Banner
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBWMsN8BL6xn8WtM_JNwharHRGLFYwmNIo",
+    authDomain: "reactflix-831e2.firebaseapp.com",
+    projectId: "reactflix-831e2",
+    storageBucket: "reactflix-831e2.appspot.com",
+    messagingSenderId: "917661518946",
+    appId: "1:917661518946:web:c3ebd95c0f46088aef153b"
+  };
