b5fff6a047d63685e8517bba2cf1f84999b30d40
Vikash Kumar Mahto <emails2vikash@gmail.com>
2023-01-29 15:24:50 +0530

    Banner work half completed

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,54 +2,36 @@ import React from 'react'
 import './Banner.css'
 
 const Banner = () => {
+
+  function truncate(str,n){
+    return str.length > n ? str.substring(0,n-1) + '.....' : str;
+  }
   return (
-    <div className='banner'>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-    </div>
+    <header className='banner' 
+    style={{
+      backgroundSize: 'cover',
+      backgroundImage: `url("https://wallpapercave.com/wp/wp8741529.jpg")`,
+      backgroundPosition: 'center center'
+    }}>
+      
+      <div className="bannerContent">
+        <h1 className="bannerTitle">Movie Name</h1>
+        <div className="bannerButtons">
+          <button className='bannerBtn'>Play</button>
+          <button className='bannerBtn'>My List</button>
+        </div>
+        <div className="bannerDescription">
+          {truncate(`This is a test Description
+        This is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test Description
+        This is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test Description
+        This is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test Description
+        This is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test Description
+        This is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test Description`,200)}
+        </div>
+      </div>
+
+      <div className="bannerFadeBottom" />
+    </header>
   )
 }
 
