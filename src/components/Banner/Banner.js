f65afdbe9e8a352d3ed33d4e13959b92431ec8df
Vikash Kumar Mahto <emails2vikash@gmail.com>
2023-01-31 14:44:30 +0530

    TMDB Api integration done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
+import axios from '../../api/axios.js'
+import requests from '../../api/requests';
 
 const Banner = () => {
+
+  const [movie,setMovie] = useState([]);
+
+  useEffect(()=>{
+    async function fetchData(){
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  },[]);
+
+  console.log(movie);
 
   function truncate(str,n){
     return str.length > n ? str.substring(0,n-1) + '.....' : str;
@@ -10,23 +29,20 @@ const Banner = () => {
     <header className='banner' 
     style={{
       backgroundSize: 'cover',
-      backgroundImage: `url("https://wallpapercave.com/wp/wp8741529.jpg")`,
+      backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
       backgroundPosition: 'center center'
     }}>
       
       <div className="bannerContent">
-        <h1 className="bannerTitle">Movie Name</h1>
+        <h1 className="bannerTitle">
+          { movie?.title || movie?.name || movie?.original_name }
+        </h1>
         <div className="bannerButtons">
           <button className='bannerBtn'>Play</button>
           <button className='bannerBtn'>My List</button>
         </div>
         <div className="bannerDescription">
-          {truncate(`This is a test Description
-        This is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test Description
-        This is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test Description
-        This is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test Description
-        This is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test Description
-        This is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test Description`,200)}
+          {truncate(`${movie?.overview}`,200)}
         </div>
       </div>
 
